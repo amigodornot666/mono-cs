88af32fb7db1a47a601a675f2cc1c28e4436eb1d
Kyle Roux <decimalwarrior@gmail.com>
2012-07-22 16:43:07 -0700

    added first c# source file and exe. make directory structure as well.

---
+++ mono/src/variables.cs
@@ -0,0 +1,11 @@
+// just a quck demo to assign a variable
+// variable.cs
+using System;
+
+class variable {
+	public static void Main() {
+		int x = 3;
+		Console.WriteLine("The value of x is " + x);
+	}
+}
+
