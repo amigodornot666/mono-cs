ed7728562772495e37f8f6f8356d0b380edd8fed
Kyle Roux <decimalwarrior@gmail.com>
2012-07-22 16:51:02 -0700

    added a test of variable assignment and printing with brackets. I learned a lot here.

---
+++ mono/src/vari2.cs
@@ -0,0 +1,12 @@
+// testing how brackets work with string operators
+using System;
+
+class Vari2 {
+	public static void Main() {
+		uint x = 3;
+		Console.WriteLine("The value of x + 1 is " + x + 1);
+		Console.WriteLine("The value of (x + 1) is " + (x + 1));
+	}
+}
+
+
