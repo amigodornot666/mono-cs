3826939f209816046f02d71f5e1a587849f6c807
amigodornot666 <decimalwarrior@gmail.com>
2012-07-23 14:04:47 -0700

    added a for loop demo. for.cs

---
+++ mono/src/for1.cs
@@ -0,0 +1,15 @@
+// for loop demo
+// for1.cs
+using System;
+
+class loop1 {
+				public static void Main() {
+								int i;
+								for 	(i = 0; i < 100000; i++) {
+												Console.WriteLine("Current Value: " + i);
+								}
+				}
+}
+				
+
+
