f5706a14a17dc9651a92a6a21ae463ee30b770b4
Kyle Roux <decimalwarrior@gmail.com>
2012-07-23 16:00:29 -0700

    added another loop.

---
+++ mono/src/loop.cs
@@ -0,0 +1,14 @@
+// a for loop demo
+// loop.cs
+using System;
+
+public class Loop {
+	public static void Main() {
+		int i;
+
+		for (i=0; i<100;)	
+			Console.WriteLine("now its: " + i);
+			i++;
+		}
+}
+
