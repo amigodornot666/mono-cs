e6670d6232a780b3a7ee8c7ec419d07207229fc4
amigodornot666 <decimalwarrior@gmail.com>
2012-07-23 14:22:23 -0700

    added another for loop demo while2.cs

---
+++ mono/src/while2.cs
@@ -0,0 +1,15 @@
+// 2nd while loop demo
+// while2.cs
+using System;
+
+class While2 {
+				public static void Main() {
+								int i;
+								for 	(i = 0; i < 100000; ) {
+												Console.WriteLine("Current Value: " + i);
+												i++;
+								}
+				}
+}
+
+
