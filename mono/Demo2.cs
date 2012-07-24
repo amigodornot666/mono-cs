cde4d4c597bdbfda1b5d69261eb3a612f5bddbe2
Kyle Roux <decimalwarrior@gmail.com>
2012-07-23 17:25:58 -0700

    added a class demo

---
+++ mono/Demo2.cs
@@ -0,0 +1,17 @@
+// quick class demo
+// Demo2.cs
+using System;
+
+class Demo {
+	public static void Main() {
+		Human hugo = new Human();
+	}
+}
+
+// the class itself
+public class Human {
+	public Human() {
+		Console.WriteLine("I was born to FUCK!!!");
+	}
+}
+
