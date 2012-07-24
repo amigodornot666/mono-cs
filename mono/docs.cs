fda177f9c7c063837d1b8ec8858907e677137502
Kyle Roux <decimalwarrior@gmail.com>
2012-07-23 17:00:07 -0700

    added a doc sample

---
+++ mono/docs.cs
@@ -0,0 +1,14 @@
+// quick doc example
+// docs.cs
+using System;
+
+class MyLoop {
+	public static void Main() {
+		/// <summary>
+		///	This is a free fuck off
+		/// 	at suckadick.com
+		/// </summary>
+		Console.WriteLine("Go to Fuckoff.com");
+		/// <seealso> www.asslick.com </seealso>
+	}
+}
